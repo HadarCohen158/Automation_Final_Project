058adbcff3a7b027a9947804b94eb7d91329bb54
hadar cohen <hadarco158@gmail.com>
2023-11-09 22:39:37 +0200

    refactoring

--- src/test/java/tests/sanityTest/SanityScenarioTest.java
+++ src/test/java/tests/sanityTest/SanityScenarioTest.java
@@ -12,185 +12,88 @@ public class SanityScenarioTest {
 
     public static void main(String[] args) throws InterruptedException {
 
-        Faker fakeDataGenerator = new Faker();
+        WebDriver driver = setupWebDriver();
+        String titleCssSelector = "[class=\"title\"]";
+        String domainName = "www.saucedemo.com";
 
-        // WebDriver setup
-        //WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.saucedemo.com");
+        login(driver);
 
-        //Login with the user standard_user
-        WebElement usernameField = driver.findElement(By.id("user-name"));
-        usernameField.sendKeys("standard_user");
+        validateLoginURL(driver);
 
-        //Login with the password   secret_sauce
-        WebElement passwordField = driver.findElement(By.id("password"));
-        passwordField.sendKeys("secret_sauce");
-        driver.findElement(By.cssSelector("[class=\"submit-button btn_action\"]")).click();
+        validatePageTitle(driver, titleCssSelector, "Products");
 
-        //Validate (using if-else statement) the URL of the page: https://www.saucedemo.com/inventory.html
-        //url check
-        String currentUrl = driver.getCurrentUrl();
-        System.out.println("currentUrl = " + currentUrl);
-        String expectedUrl = "https://www.saucedemo.com/inventory.html";
+        addAndVerifyTwoProducts(driver);
 
-        if (currentUrl.equals(expectedUrl)) {
-            System.out.println("the url is corect");
-        } else {
-            System.out.println("the url is worng");
-        }
+        urlCheckByCssSelector(driver, "[class=\"shopping_cart_link\"]", domainName, "cart.html");
 
-        //• Validate (using if-else statement) the title of the page. בעיה
-        String productTitle = driver.findElement(By.cssSelector("[class=\"title\"]")).getText();
-        String expectedTitle = "Products";
-        System.out.println(productTitle);
+        validatePageTitle(driver, titleCssSelector, "Your Cart");
 
-        if (productTitle.equals(expectedTitle)) {
-            System.out.println(expectedTitle + " this is the correct title ");
-        } else {
-            System.out.println(expectedTitle + "this is  not the correct ");
-        }
+        validateProductNumberInCart(driver);
 
-        //  Add 2 products by using their names
-        WebElement buttonElement1 = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
-        buttonElement1.click();
+        urlCheckById(driver, "checkout", domainName, "checkout-step-one.html");
 
-        WebElement buttonElement2 = driver.findElement(By.id("add-to-cart-sauce-labs-fleece-jacket"));
-        buttonElement2.click();
+        validatePageTitle(driver, titleCssSelector, "Checkout: Your Information");
 
-        // Validate the cart icon presents a correct number of items (2).
-        String numberOfItems = driver.findElement(By.cssSelector("[class=\"shopping_cart_badge\"]")).getText();
-        String expectedNumberOfItemsText = "2";
+        fillPersonalDetailsForm(driver);
 
-        if (numberOfItems.equals(expectedNumberOfItemsText)) {
-            System.out.println(expectedNumberOfItemsText +" "+ "this is the correct number");
-        } else {
-            System.out.println(expectedNumberOfItemsText +" "+ "this is  not the correct number");
-        }
-        //     Click the icon to navigate to the next step.
+        checkout2UrlCheck(driver);
 
-        driver.findElement(By.cssSelector("[class=\"shopping_cart_link\"]")).click();
+        validatePageTitle(driver, titleCssSelector, "Checkout: Overview");
 
-        //url check
+        urlCheckById(driver, "finish", domainName, "checkout-complete.html");
+
+        validatePageTitle(driver, titleCssSelector, "Checkout: Complete!");
+
+        validateUserCompleteMessage(driver);
+    }
+
+    private static void urlCheckByCssSelector(WebDriver driver, String cssSelector, String domain, String url) {
+        // Click the icon to navigate to the next step.
+        driver.findElement(By.cssSelector(cssSelector)).click();
+
+        // url check
         String cartUrl = driver.getCurrentUrl();
-        System.out.println("currentUrl = " + cartUrl);
-        String theCartUrl = "https://www.saucedemo.com/cart.html";
+        System.out.println("currentUrl = " + url);
+        String doaminName = domain + "/" + url;
+        String theCartUrl = String.format("https://%s", doaminName);
 
         if (cartUrl.equals(theCartUrl)) {
             System.out.println(cartUrl +" "+ "the url is corect");
         } else {
             System.out.println(cartUrl +" "+ "the url is worng");
         }
+    }
 
-        // Validate page title: Your Cart
-        String title = driver.findElement(By.cssSelector("[class=\"title\"]")).getText();
+    private static void urlCheckById(WebDriver driver, String id, String domain, String url) {
+        // Click the icon to navigate to the next step.
+        driver.findElement(By.id(id)).click();
 
-        String titleOfCart = "Your Cart";
-        System.out.println(titleOfCart);
+        // url check
+        String cartUrl = driver.getCurrentUrl();
+        System.out.println("currentUrl = " + url);
+        String doaminName = domain + "/" + url;
+        String theCartUrl = String.format("https://%s", doaminName);
 
-        if (title.equals(titleOfCart)) {
-            System.out.println(titleOfCart +" " + "this is the correct title ");
+        if (cartUrl.equals(theCartUrl)) {
+            System.out.println(cartUrl +" "+ "the url is corect");
         } else {
-            System.out.println(titleOfCart +" " + "this is  not the correct ");
+            System.out.println(cartUrl +" "+ "the url is worng");
         }
+    }
 
-        //    Validate a correct number of items (2)
-        WebElement cartItemList = driver.findElement(By.cssSelector("[class=\"cart_list\"]"));
+    private static void validatePageTitle(WebDriver driver, String cssSelector, String pageTitle ) {
+        //• Validate (using if-else statement) the title of the page
+        String productTitle = driver.findElement(By.cssSelector(cssSelector)).getText();
+        System.out.println(productTitle);
 
-        java.util.List<WebElement> cartItems = cartItemList.findElements(By.className("cart_item"));
-
-        int itemCount = cartItems.size();
-
-        System.out.println("the number of the items is: " + itemCount);
-
-        //• Click the Checkout button to navigate to the next step
-        driver.findElement(By.id("checkout")).click();
-
-        //Validate an URL: https://www.saucedemo.com/checkout-step-one.html
-        String checkOutUrl = driver.getCurrentUrl();
-        System.out.println("currentUrl = " + checkOutUrl);
-
-        String theCheckoutUrl = "https://www.saucedemo.com/checkout-step-one.html";
-
-        if (theCheckoutUrl.equals(checkOutUrl)) {
-            System.out.println(theCheckoutUrl + "the url is corect");
+        if (productTitle.equals(pageTitle)) {
+            System.out.println(pageTitle + " this is the correct title ");
         } else {
-            System.out.println(theCheckoutUrl + "the url is worng");
+            System.out.println(pageTitle + "this is  not the correct ");
         }
+    }
 
-        //  Validate page title: Checkout: Your Information
-        String CheckoutTitle = driver.findElement(By.cssSelector("[class=\"title\"]")).getText();
-        String expectedCheckoutTitle = "Checkout: Your Information";
-
-        if (CheckoutTitle.equals(expectedCheckoutTitle)) {
-            System.out.println(expectedCheckoutTitle + " " + "this is the correct message ");
-        } else {
-            System.out.println(expectedCheckoutTitle + " "+ "this is  not the correct ");
-        }
-
-        //Fill The Form first name
-        driver.findElement(By.id("first-name")).sendKeys(fakeDataGenerator.name().firstName());
-
-        //Fill The Form last name
-        driver.findElement(By.id("last-name")).sendKeys(fakeDataGenerator.name().lastName());
-
-        //Fill The Form zip code
-        driver.findElement(By.id("postal-code")).sendKeys(fakeDataGenerator.address().zipCode());
-
-        //• Click the Continue button to navigate to the next step
-        driver.findElement(By.id("continue")).click();
-
-        // Validate an URL: https://www.saucedemo.com/checkout-step-two.html
-        String theOverviewUrl = driver.getCurrentUrl();
-        System.out.println("currentUrl = " + theOverviewUrl);
-
-        String theOverviewUrlOfTHePage = "https://www.saucedemo.com/checkout-step-two.html";
-
-        if (theOverviewUrl.equals(theOverviewUrlOfTHePage)) {
-            System.out.println("the url is corect");
-        } else {
-            System.out.println("the url is worng");
-        }
-
-        // Validate page title: Checkout: Overview
-        String OverviewTitle = driver.findElement(By.cssSelector("[class=\"title\"]")).getText();
-
-        String expectedOverviewTitlePage = "Checkout: Overview";
-        System.out.println(expectedOverviewTitlePage);
-
-        if (OverviewTitle.equals(expectedOverviewTitlePage)) {
-            System.out.println(expectedOverviewTitlePage + " "+ "this is the correct massage ");
-        } else {
-            System.out.println(expectedOverviewTitlePage + " " + "this is  not the correct massage ");
-        }
-
-        // Click the Finish button to navigate to the next step
-        driver.findElement(By.id("finish")).click();
-
-        // Validate an URL: https://www.saucedemo.com/checkout-complete.html
-        String completeUrl = driver.getCurrentUrl();
-        System.out.println("currentUrl = " + completeUrl);
-
-        String completeCheckoutUrl = "https://www.saucedemo.com/checkout-complete.html";
-
-        if (completeUrl.equals(completeCheckoutUrl)) {
-            System.out.println("the complete url is corect");
-        } else {
-            System.out.println("the complete url is worng");
-        }
-
-        //Validate page title: Checkout: Complete!
-        String CompleteTitle = driver.findElement(By.cssSelector("[class=\"title\"]")).getText();
-        String expectedCompleteTitleTitlePage = "Checkout: Complete!";
-        System.out.println(expectedCompleteTitleTitlePage);
-
-        if (CompleteTitle.equals(expectedCompleteTitleTitlePage)) {
-            System.out.println(expectedCompleteTitleTitlePage + " " + " this is the correct massage ");
-        } else {
-            System.out.println(expectedCompleteTitleTitlePage+ " "  + "this is not the correct massage ");
-        }
-
+    private static void validateUserCompleteMessage(WebDriver driver) {
         // Validate a text of each one of the presented messages
         String thankYou = driver.findElement(By.cssSelector("[class=\"complete-header\"]")).getText();
         String expectedThankYouTitlePage = "Thank you for your order!";
@@ -211,5 +114,94 @@ public class SanityScenarioTest {
         } else {
             System.out.println(expectedCompleteTextPage + "the messages is  notcorrect ");
         }
+    }
+
+    private static void checkout2UrlCheck(WebDriver driver) {
+        // Validate an URL: https://www.saucedemo.com/checkout-step-two.html
+        String theOverviewUrl = driver.getCurrentUrl();
+        System.out.println("currentUrl = " + theOverviewUrl);
+
+        String theOverviewUrlOfTHePage = "https://www.saucedemo.com/checkout-step-two.html";
+
+        if (theOverviewUrl.equals(theOverviewUrlOfTHePage)) {
+            System.out.println("the url is corect");
+        } else {
+            System.out.println("the url is worng");
+        }
+    }
+
+    private static void fillPersonalDetailsForm(WebDriver driver) {
+        Faker fakeDataGenerator = new Faker();
+
+        //Fill The Form first name
+        driver.findElement(By.id("first-name")).sendKeys(fakeDataGenerator.name().firstName());
+
+        //Fill The Form last name
+        driver.findElement(By.id("last-name")).sendKeys(fakeDataGenerator.name().lastName());
+
+        //Fill The Form zip code
+        driver.findElement(By.id("postal-code")).sendKeys(fakeDataGenerator.address().zipCode());
+
+        //• Click the Continue button to navigate to the next step
+        driver.findElement(By.id("continue")).click();
+    }
+
+    private static void validateProductNumberInCart(WebDriver driver) {
+        // Validate a correct number of items (2)
+        WebElement cartItemList = driver.findElement(By.cssSelector("[class=\"cart_list\"]"));
+        java.util.List<WebElement> cartItems = cartItemList.findElements(By.className("cart_item"));
+        int itemCount = cartItems.size();
+        System.out.println("the number of the items is: " + itemCount);
+    }
+
+    private static void addAndVerifyTwoProducts(WebDriver driver) {
+        //  Add 2 products by using their names
+        WebElement buttonElement1 = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
+        buttonElement1.click();
+
+        WebElement buttonElement2 = driver.findElement(By.id("add-to-cart-sauce-labs-fleece-jacket"));
+        buttonElement2.click();
+
+        // Validate the cart icon presents a correct number of items (2).
+        String numberOfItems = driver.findElement(By.cssSelector("[class=\"shopping_cart_badge\"]")).getText();
+        String expectedNumberOfItemsText = "2";
+
+        if (numberOfItems.equals(expectedNumberOfItemsText)) {
+            System.out.println(expectedNumberOfItemsText +" "+ "this is the correct number");
+        } else {
+            System.out.println(expectedNumberOfItemsText +" "+ "this is  not the correct number");
+        }
+    }
+
+    private static void validateLoginURL(WebDriver driver) {
+        // Validate (using if-else statement) the URL of the page: https://www.saucedemo.com/inventory.html
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println("currentUrl = " + currentUrl);
+        String expectedUrl = "https://www.saucedemo.com/inventory.html";
+
+        if (currentUrl.equals(expectedUrl)) {
+            System.out.println("the url is corect");
+        } else {
+            System.out.println("the url is worng");
+        }
+    }
+
+    private static void login(WebDriver driver) {
+        //Login with the user standard_user
+        WebElement usernameField = driver.findElement(By.id("user-name"));
+        usernameField.sendKeys("standard_user");
+
+        //Login with the password   secret_sauce
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("[class=\"submit-button btn_action\"]")).click();
+    }
+
+    private static WebDriver setupWebDriver() {
+        // WebDriver setup
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com");
+        return driver;
     }
 }
