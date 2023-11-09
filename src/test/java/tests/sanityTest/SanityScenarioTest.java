e09007e53f2d291c2ddf37f1e0469fea4dafd6ad
hadar cohen <hadarco158@gmail.com>
2023-11-09 20:17:36 +0200

    refactoring

--- src/test/java/tests/sanityTest/SanityScenarioTest.java
+++ src/test/java/tests/sanityTest/SanityScenarioTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-public class SanityScenario {
+public class SanityScenarioTest {
 
 
     public static void main(String[] args) throws InterruptedException {
